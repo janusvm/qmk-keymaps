b314d6062bc1f1f299fd6148ad0aa99698d292b1
Janus S. Valberg-Madsen <janusvm@gmail.com>
2022-06-09 21:15:49 +0200

    ✨ Add symbols layer to kbd67

--- kbd67r2/keymap.c
+++ kbd67r2/keymap.c
@@ -5,14 +5,17 @@
 enum kbd67_layers {
   _COLEMAK_DH,
   _QWERTY,
+  _SYMBOLS,
   _FN
 };
 
-#define KC_FN MO(_FN)
+#define SYMBOLS MO(_SYMBOLS)
+#define FN      MO(_FN)
 #define OSMLSFT OSM(MOD_LSFT)
 #define OSMRSFT OSM(MOD_RSFT)
 #define OSMLCTL OSM(MOD_LCTL)
 #define OSMRCTL OSM(MOD_RCTL)
+#define MY_EURO ALGR(DK_E)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK_DH] = LAYOUT_65_iso_split_space(
@@ -20,7 +23,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  /*****/  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    DK_DIAE, KC_J,    KC_L,    KC_U,    KC_Y,    DK_AE,   DK_ARNG, /*****/  KC_PGUP,
         OSMLCTL, /*****/  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    DK_QUOT, KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    DK_OSTR, KC_ENT,  KC_PGDN,
         OSMLSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    DK_LABK, DK_MINS, KC_K,    KC_H,    KC_COMM, KC_DOT,  /*****/  OSMRSFT, KC_UP,   KC_END,
-        CAPSWRD, KC_LGUI, /*****/  KC_LALT, KC_SPC,  /*****/  KC_ALGR, /*****/  KC_BSPC, /*****/  KC_RGUI, KC_FN,   OSMRCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        CAPSWRD, KC_LGUI, /*****/  KC_LALT, KC_SPC,  /*****/  SYMBOLS, /*****/  KC_BSPC, /*****/  KC_RGUI, FN,      OSMRCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        ),
+    [_SYMBOLS] = LAYOUT_65_iso_split_space(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, /*****/  _______, _______,
+        _______, /*****/  DK_EXLM, DK_AT,   DK_LCBR, DK_RCBR, DK_PERC, DK_DIAE, DK_AMPR, DK_LABK, DK_RABK, DK_DQUO, DK_EQL,  DK_CURR, /*****/  _______,
+        _______, /*****/  DK_CIRC, DK_DLR,  DK_LBRC, DK_RBRC, DK_HASH, DK_ACUT, DK_ASTR, DK_LPRN, DK_RPRN, DK_QUOT, DK_PLUS, DK_SLSH, _______, _______,
+        _______, DK_BSLS, DK_PIPE, DK_GRV,  DK_TILD, DK_QUES, DK_PND,  DK_EURO, DK_MINS, DK_UNDS, DK_HALF, DK_SECT, /*****/  _______, _______, _______,
+        _______, _______, /*****/  _______, _______, /*****/  _______, /*****/  _______, /*****/  _______, _______, _______, _______, _______, _______
         ),
     [_FN] = LAYOUT_65_iso_split_space(
         QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  /*****/  _______, KC_PSCR,
