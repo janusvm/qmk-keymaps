a56a499e6a0029df720fab10c9de11d83bca22d6
Janus S. Valberg-Madsen <janusvm@gmail.com>
2022-06-16 19:32:23 +0200

    🐛 Fix the TAPPING_TERM for the Planck

--- planck/config.h
+++ planck/config.h
@@ -20,7 +20,7 @@
 #define ONESHOT_TIMEOUT 1500
 
 // Settings for home row mods
-#define TAPPING_TERM 175
+#define TAPPING_TERM 150
 #define TAPPING_TERM_PER_KEY
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD_PER_KEY
