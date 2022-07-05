065119acd83a263ad97c7983fae615a3a5af6104
Janus S. Valberg-Madsen <janusvm@gmail.com>
2022-07-05 20:14:40 +0200

    ⏪ Change Planck keymap back to use home row mods

--- planck/keymap.c
+++ planck/keymap.c
@@ -15,21 +15,8 @@ enum planck_layers {
 #define FN_COMM    LT(_FN, KC_COMM)
 #define LOWER_SPC  LT(_LOWER, KC_SPC)
 #define RAISE_BSPC LT(_RAISE, KC_BSPC)
-#define ACCENTS    OSL(_ACCENTS)
-
-// Mod tap keys
-#define G_Z    GUI_T(KC_Z)
-#define A_X    ALT_T(KC_X)
-#define A_DOT  ALT_T(KC_DOT)
-#define G_SLSH GUI_T(KC_SLSH)
-#define C_ESC  CTL_T(KC_ESC)
-#define S_ENT  SFT_T(KC_ENT)
-
-// One shot mods
-#define OSM_SFT OSM(MOD_LSFT)
-#define OSM_CTL OSM(MOD_LCTL)
-#define OSM_ALT OSM(MOD_LALT)
-#define OSM_GUI OSM(MOD_LGUI)
+#define FN_TAB     LT(_FN, KC_TAB)
+#define ACCENT_ESC LT(_ACCENTS, KC_ESC)
 
 // Combined keys
 #define C_Z C(KC_Z)
@@ -42,42 +29,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // COLEMAK-DH layer, based on ANSI Colemak-DHm
     [_COLEMAK_DH] = LAYOUT(
-        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,      XXXXXXX, XXXXXXX, KC_J,       KC_L,    KC_U,    KC_Y,    KC_DEL,
-        KC_A,    KC_R,    KC_S,    KC_T,    KC_G,      XXXXXXX, XXXXXXX, KC_M,       KC_N,    KC_E,    KC_I,    KC_O,
-        G_Z,     A_X,     KC_C,    KC_D,    KC_V,      XXXXXXX, XXXXXXX, KC_K,       KC_H,    FN_COMM, A_DOT,   G_SLSH,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LOWER_SPC, OSM_CTL, OSM_SFT, RAISE_BSPC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+        KC_Q,        KC_W,        KC_F,        KC_P,        KC_B,      XXXXXXX, XXXXXXX,    KC_J,       KC_L,        KC_U,        KC_Y,        KC_DEL,
+        GUI_T(KC_A), ALT_T(KC_R), CTL_T(KC_S), SFT_T(KC_T), KC_G,      XXXXXXX, XXXXXXX,    KC_M,       SFT_T(KC_N), CTL_T(KC_E), ALT_T(KC_I), GUI_T(KC_O),
+        KC_Z,        KC_X,        KC_C,        KC_D,        KC_V,      XXXXXXX, XXXXXXX,    KC_K,       KC_H,        KC_COMM,     KC_DOT,      KC_ENT,
+        XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     LOWER_SPC, FN_TAB,  ACCENT_ESC, RAISE_BSPC, XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX
     ),
 
     // LOWER layer, for numbers, navigation and Danish characters
     [_LOWER] = LAYOUT(
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX, XXXXXXX, KC_PGUP, KC_HOME, KC_UP,   KC_END,   KC_ESC,
-        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX, XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_ENT,
-        OSM_GUI, OSM_ALT, OSM_CTL, KC_TAB,  KC_ESC,  XXXXXXX, XXXXXXX, CAPSWRD, KC_AE,   KC_OE,   KC_AA,    OSM_CTL,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX
+        KC_1,        KC_2,        KC_3,        KC_4,        KC_5,    XXXXXXX, XXXXXXX, KC_PGUP, KC_HOME, KC_UP,   KC_END,   XXXXXXX,
+        GUI_T(KC_6), ALT_T(KC_7), CTL_T(KC_8), SFT_T(KC_9), KC_0,    XXXXXXX, XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX,
+        C_Z,         C_X,         C_C,         C_V,         C_S,     XXXXXXX, XXXXXXX, XXXXXXX, KC_AE,   KC_OE,   KC_AA,    XXXXXXX,
+        XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     _______, _______, KC_ENT,  _______, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX
     ),
 
     // RAISE layer, for special characters
     [_RAISE] = LAYOUT(
         KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PERC, XXXXXXX, XXXXXXX, KC_AMPR, KC_LT,   KC_GT,   KC_DQUO, KC_EQL,
         KC_CIRC, KC_DLR,  KC_LBRC, KC_RBRC, KC_HASH, XXXXXXX, XXXXXXX, KC_ASTR, KC_LPRN, KC_RPRN, KC_QUOT, KC_PLUS,
-        KC_BSLS, KC_PIPE, KC_GRV,  KC_TILD, KC_QUES, XXXXXXX, XXXXXXX, KC_ENT,  KC_UNDS, KC_SCLN, KC_COLN, KC_MINS,
+        KC_BSLS, KC_PIPE, KC_GRV,  KC_TILD, KC_QUES, XXXXXXX, XXXXXXX, KC_SLSH, KC_UNDS, KC_SCLN, KC_COLN, KC_MINS,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
     // ADJUST layer, with mouse keys and board settings
     [_ADJUST] = LAYOUT(
         RESET,   XXXXXXX, KC_BRID, KC_BRIU, RGB_TOG, XXXXXXX, XXXXXXX, UC_M_WC, KC_CAPS, KC_SCRL, KC_NUM,  KC_INS,
-        XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, AU_TOG,  XXXXXXX, XXXXXXX, UC_M_MA, KC_PSCR, KC_APP,  XXXXXXX, KC_RALT,
-        C_Z,     C_X,     C_C,     C_V,     C_S,     XXXXXXX, XXXXXXX, UC_M_LN, KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT,
+        XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, AU_TOG,  XXXXXXX, XXXXXXX, UC_M_MA, KC_WBAK, KC_WFWD, KC_WHOM, KC_WREF,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UC_M_LN, KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
-    // FN layer, for F-keys
+    // FN layer, for F-keys and numpad
     [_FN] = LAYOUT(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, ACCENTS, _______, KC_RALT, KC_RGUI,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+        KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5,   XXXXXXX, XXXXXXX, KC_COMM, KC_P7,        KC_P8,        KC_P9,        KC_PDOT,
+        GUI_T(KC_F6), ALT_T(KC_F7), CTL_T(KC_F8), SFT_T(KC_F9), KC_F10,  XXXXXXX, XXXXXXX, KC_PAST, SFT_T(KC_P4), CTL_T(KC_P5), ALT_T(KC_P6), GUI_T(KC_PPLS),
+        KC_F11,       KC_F12,       KC_APP,       KC_PSCR,      CAPSWRD, XXXXXXX, XXXXXXX, KC_PSLS, KC_P1,        KC_P2,        KC_P3,        KC_PMNS,
+        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      _______, _______, KC_ENT,  KC_P0,   XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
     ),
 
     // ACCENTS layer, for accented vowels
@@ -85,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV_A, KC_DIA_A, KC_CIR_A, KC_TIL_A, KC_ACU_A, XXXXXXX, XXXXXXX, KC_GRV_O, KC_DIA_O, KC_CIR_O, KC_TIL_O, KC_ACU_O,
         KC_GRV_E, KC_DIA_E, KC_CIR_E, KC_TIL_E, KC_ACU_E, XXXXXXX, XXXXXXX, KC_GRV_U, KC_DIA_U, KC_CIR_U, KC_TIL_U, KC_ACU_U,
         KC_GRV_I, KC_DIA_I, KC_CIR_I, KC_TIL_I, KC_ACU_I, XXXXXXX, XXXXXXX, KC_GRV_Y, KC_DIA_Y, KC_CIR_Y, KC_TIL_Y, KC_ACU_Y,
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______, _______, _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______, _______, KC_RALT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
     )
 };
 
@@ -120,21 +107,6 @@ void keyboard_post_init_user(void) {
     rgblight_setrgb(RGB_OFF);
 }
 
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case G_Z:
-        case A_X:
-        case FN_COMM:
-        case A_DOT:
-        case G_SLSH:
-            // Longer tapping term for main keys
-            return TAPPING_TERM * 2;
-        default:
-            // Short tapping term for thumb keys
-            return TAPPING_TERM;
-    }
-}
-
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case RAISE_BSPC:
@@ -149,6 +121,8 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LOWER_SPC:
         case RAISE_BSPC:
+        case FN_TAB:
+        case ACCENT_ESC:
             // Enable HOLD_ON_OTHER_KEY_PRESS for just the thumb keys
             return true;
         default:
