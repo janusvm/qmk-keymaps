12242a8453d6f92e23726bc9ce411d8ac47a5dbc
Janus S. Valberg-Madsen <janusvm@gmail.com>
2022-07-07 22:08:47 +0200

    🐛 Swap numpad dot for regular dot on Planck
    Numpad dot just doesn't work for me, probably due to OS locale settings.

--- planck/keymap.c
+++ planck/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // FN layer, for F-keys and numpad
     [_FN] = LAYOUT(
-        KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5,   XXXXXXX, XXXXXXX, KC_COMM, KC_P7,        KC_P8,        KC_P9,        KC_PDOT,
+        KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5,   XXXXXXX, XXXXXXX, KC_COMM, KC_P7,        KC_P8,        KC_P9,        KC_DOT,
         GUI_T(KC_F6), ALT_T(KC_F7), CTL_T(KC_F8), SFT_T(KC_F9), KC_F10,  XXXXXXX, XXXXXXX, KC_PAST, SFT_T(KC_P4), CTL_T(KC_P5), ALT_T(KC_P6), GUI_T(KC_PPLS),
         KC_F11,       KC_F12,       KC_APP,       KC_PSCR,      CAPSWRD, XXXXXXX, XXXXXXX, KC_PSLS, KC_P1,        KC_P2,        KC_P3,        KC_PMNS,
         XXXXXXX,      XXXXXXX,      XXXXXXX,      _______,      XXXXXXX, XXXXXXX, XXXXXXX, KC_ENT,  KC_P0,        XXXXXXX,      XXXXXXX,      XXXXXXX
