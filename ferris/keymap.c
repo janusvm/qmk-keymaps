32fe20f94e5a072814ded975706838c5d94399e4
Janus Valberg-Madsen <janusvm@gmail.com>
2022-10-22 23:38:36 +0200

    🐛 Force Sweep to start on Colemak layer
    Add a `keyboard_post_init_user` function to Ferris Sweep layout that
    sets the persistent base layer to the `_COLEMAK` layer.
    
    This is necessary as this layer is numbered as 1 in the shared enum, and
    the Sweep layout doesn't include the QWERTY (0) layer.

--- ferris/keymap.c
+++ ferris/keymap.c
@@ -69,6 +69,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
+void keyboard_post_init_user(void) {
+    set_single_persistent_default_layer(_COLEMAK);
+}
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
